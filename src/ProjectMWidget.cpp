05c69648f4844662d3cf06f2bea0275150b3d413
nsomnia <sderek02@gmail.com>
2025-12-08 20:11:22 -0700

    gemini3.5 improvments

--- src/ProjectMWidget.cpp
+++ src/ProjectMWidget.cpp
@@ -1,27 +1,69 @@
 #include "ProjectMWidget.h"
+
 #include <QOpenGLFunctions>
+#include <QDir>
+#include <QCoreApplication>
 
 ProjectMWidget::ProjectMWidget(QWidget *parent)
     : QOpenGLWidget(parent)
+    , m_projectM(nullptr)
+    , m_playlist(nullptr)
 {
 }
 
 ProjectMWidget::~ProjectMWidget()
 {
+    if (m_playlist)
+    {
+        projectm_playlist_destroy(m_playlist);
+    }
+    if (m_projectM)
+    {
+        projectm_destroy(m_projectM);
+    }
 }
 
 void ProjectMWidget::initializeGL()
 {
     initializeOpenGLFunctions();
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+    m_projectM = projectm_create();
+    if (!m_projectM)
+    {
+        // Handle error
+        return;
+    }
+
+    projectm_set_window_size(m_projectM, width(), height());
+    projectm_set_mesh_size(m_projectM, 1024, 768);
+
+    projectm_set_fps(m_projectM, 30);
+
+    const char* presetPath = "build/_deps/projectm-src/presets";
+    const char* texturePath = "build/_deps/projectm-src/textures";
+    
+    m_playlist = projectm_playlist_create(m_projectM);
+    projectm_playlist_add_path(m_playlist, presetPath, true, true);
+
+    projectm_set_texture_search_paths(m_projectM, &texturePath, 1);
+
 }
 
 void ProjectMWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    if (m_projectM)
+    {
+        projectm_opengl_render_frame(m_projectM);
+    }
 }
 
 void ProjectMWidget::resizeGL(int w, int h)
 {
     glViewport(0, 0, w, h);
+    if (m_projectM)
+    {
+        projectm_set_window_size(m_projectM, w, h);
+    }
 }
--- src/ProjectMWidget.h
+++ src/ProjectMWidget.h
@@ -4,6 +4,9 @@
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 
+#include <projectM-4/projectM.h>
+#include <projectM-4/playlist.h>
+
 class ProjectMWidget : public QOpenGLWidget, protected QOpenGLFunctions
 {
     Q_OBJECT
@@ -16,6 +19,10 @@ protected:
     void initializeGL() override;
     void paintGL() override;
     void resizeGL(int w, int h) override;
+
+private:
+    projectm_handle m_projectM;
+    projectm_playlist_handle m_playlist;
 };
 
 #endif // PROJECTMWIDGET_H
